0b69fd282a2c847f7ab9f25fd3a812179f09ec6f
toohil <toohil@github.com>
2026-01-14 12:10:41 +0000

    RAG documents Including some study documentation for RAG testing. Will remove from repo later when proper implementation for submitting files is complete.

--- src/chatfunctions.js
+++ src/chatfunctions.js
@@ -1,42 +1,20 @@
 import { pipeline } from "@huggingface/transformers";
 import fs from 'fs';
 
-// const extractor = await pipeline('feature-extraction', 'Xenova/jina-embeddings-v2-small-en');
+const knowledgeDB = [];
+const extractor = await pipeline('feature-extraction', 'Xenova/jina-embeddings-v2-small-en');
 
-// let file = fs.readFileSync('SDJC01-PIL01 Stool Sample.docx.txt')
+// let file = fs.readFileSync('study_docs/SDJC01-PIL01 Stool Sample.docx.txt')
 // const instructions = file.toString().split('\n')
 
-// manual input of instructions for now, encountered issue parsing file when previewing in browser.
-const instructions = "Instructions for Collecting Stool Sample (bowel motion sample)\
-  The stool sample should be as fresh as possible, ideally produced the morning of your visit.\
-  If this is not possible, a sample from the evening/night before may be saved and stored in a fridge. This is not ideal and may interfere with analysis of the sample; therefore a sample from the morning of your visit is preferred\
-  * Sample Pack:\
-    Please find enclosed in sample pack:\
-        1. Plastic lunch-box size container with a lid (for stool sample)\
-        2. Disposable gloves\
-        3. 2 x Zip lock bag\
-        4. AnaeroGen sachet.\
-        5. Paper Envelope\
-        6. Freezer Block\
-  * Night Before Sample Gathering\
-    * Place the freezer block in the freezer overnight to freeze.\
-  * Sample Gathering:\
-    * Place the frozen freezer block in one of the ziplock bags.\
-    * Put on the disposable gloves.\
-    * Place the plastic container onto the toilet bowl and perform bowel movement into this (the whole bowel motion, not just part of it). Please avoid getting any urine in the plastic container and do not wrap or cover the sample in toilet paper.\
-    * Tear off the top of the AnaeroGen sachet which is taped to the lid of the container (see image below). Do not remove the inner sachet.\
-    * Within one minute of tearing the top of the sachet, secure the lid of the container firmly, and place the plastic container in second zip lock bag (i.e. the empty bag).\
-    * Remove and dispose of gloves.\
-    * Seal the zip lock bag, place the ziplock bag with the stool sample into the ziplock bag containing the frozen freezer block, and seal.\
-    * Place the ziplock bag (containing the freezer block, and containing the ziplock bag with the stool sample) in the paper envelope and seal.\
-    * Write down the date and the time of the stool sample on the envelope.\
-    * Place the sample in the fridge until you leave for the research lab session"
+const rag_output = await extractor(
+     instructions,
+     { pooling: 'mean' }
+);
 
+async function addToDB(input) {
 
-// const rag_output = await extractor(
-//     instructions,
-//     { pooling: 'mean' }
-// );
+}
 
 class ChatbotPipeline {
   static task = 'text-generation';
